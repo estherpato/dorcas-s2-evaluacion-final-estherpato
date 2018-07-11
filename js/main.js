1b6bcd126227a6043293aba2f4c925225176d760
Esther Pato <estherpato@gmail.com>
2018-07-11 15:56:05 +0200

    API fetch working

--- js/main.js
+++ js/main.js
@@ -3,9 +3,18 @@
 var button = document.querySelector('#button');
 var input = document.querySelector('#input');
 
+function requestFilms() {
+  var userSearch = input.value;
 
-function loquesea() {
-  console.log('soy el botón y funciono');
+  fetch('http://api.tvmaze.com/search/shows?q=' + userSearch)
+    .then(function(response){
+      return response.json();
+    })
+
+    .then(function(json){
+      var searchResult = json;
+      // for (var i = 0; i < searchResult.length)
+    });
 }
 
-button.addEventListener('click', loquesea);
+button.addEventListener('click', requestFilms);
