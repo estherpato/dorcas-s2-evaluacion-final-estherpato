7b4d9d586cf6a809ad2ea6f792be07d144dc6737
Esther Pato <estherpato@gmail.com>
2018-07-21 16:10:27 +0200

    Add intro search and modify some text

--- js/main.js
+++ js/main.js
@@ -6,21 +6,12 @@ var button = document.querySelector('#button');
 var input = document.querySelector('#input');
 var list = document.querySelector('#list');
 
-var userSearch;
-var searchResult;
-var newItem;
-var newTitle;
-var newName;
-var newIMG;
-var newURL;
-var myFilm;
-
 // función para hacer búsqueda
 function requestFilms() {
   // borramos el contenido al hacer nueva búsqueda
   list.innerHTML = '';
   // llamamos a la API con nuestra búsqueda
-  userSearch = input.value;
+  var userSearch = input.value;
   fetch('http://api.tvmaze.com/search/people?q=' + userSearch)
     .then(function(response){
       return response.json();
@@ -28,7 +19,7 @@ function requestFilms() {
 
     // Recogemos la búsqueda
     .then(function(json){
-      searchResult = json;
+      var searchResult = json;
       console.log(searchResult);
       if (searchResult.length === 0) {
         alert('Introduce una búsqueda válida (y chachi)');
@@ -36,21 +27,21 @@ function requestFilms() {
         // bucle para entrar en el array
         var resultLength = searchResult.length;
         console.log(resultLength);
-        var paragraph = document.querySelector('.searchs');
-        paragraph.innerHTML = 'Hay ' + resultLength + ' resultados para la búsqueda ' + userSearch;
+        var numberOfSearchs = document.querySelector('.number-of-searchs');
+        numberOfSearchs.innerHTML = 'Hay ' + resultLength + ' resultados para la búsqueda ' + userSearch;
         for (var i = 0; i < searchResult.length; i++) {
 
-          newItem = document.createElement('li');
+          var newItem = document.createElement('li');
           newItem.classList.add('list__item');
           // newItem.setAttribute('id', searchResult[i].show.id); // añado al li el id de backend
           newItem.addEventListener('click', favorite);
 
-          newTitle = document.createElement('h2');
+          var newTitle = document.createElement('h2');
           newTitle.classList.add('list__item-title');
-          newName = document.createTextNode(searchResult[i].person.name);
+          var newName = document.createTextNode(searchResult[i].person.name);
 
-          newIMG = document.createElement('img');
-          newURL = searchResult[i].person.image;
+          var newIMG = document.createElement('img');
+          var newURL = searchResult[i].person.image;
 
           // logica para imprimir las imágenes
           if (newURL !== null) {
@@ -71,18 +62,18 @@ function requestFilms() {
 
 // función para definir la lógica de los favoritos
 function favorite(event) {
-  myFilm = event.currentTarget;
-  // var myFilmID = myFilm.id; // meter el id en un objeto y el objeto en un array
-  if (myFilm.classList.contains('list__item--fav')) {
-    myFilm.classList.remove('list__item--fav');
-  } else {
-    myFilm.classList.add('list__item--fav');
-  }
+  var myFilm = event.currentTarget;
+  myFilm.classList.toggle('list__item--fav');
 
-console.log(myFilm.querySelector('h2').innerHTML);
+  console.log(myFilm.querySelector('h2').innerHTML);
+}
 
+// prevenir que el formulario se resetee si le doy a intro
+
+function handleFormSubmit(event) {
+  event.preventDefault();
+  console.log('hola');
 }
 
 button.addEventListener('click', requestFilms);
-
-// local storage
+button.addEventListener('click', handleFormSubmit);
